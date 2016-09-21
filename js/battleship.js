1b80d84641f4285ebc3295f7532d95a940bdeba7
Wolfgang Criollo <wolfplisken@gmail.com>
2016-09-21 14:03:31 -0400

    add the option for battleship to be place rando, fix the isSunk truthy

--- js/battleship.js
+++ js/battleship.js
@@ -1,12 +1,13 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 var guess;
 var hits = 0;
 var guesses = 0;
 var isSunk = false;
 
-while (isSunk){
+while (isSunk == false){
   guess = prompt("Ready, Aim,Fire! (enter a number from 0-6):");
   if(guess < 0 || guess > 6){
       alert("please enter a valid number");
